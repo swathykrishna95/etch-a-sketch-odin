f9e9471b41a6dd71e219b5356c3b6fb97b471d7a
Swathy Krishna <138104838+swathykrishna95@users.noreply.github.com>
2023-07-20 19:44:14 +0530

    Fill the readme details and give credit to the random color function creator

--- script.js
+++ script.js
@@ -31,10 +31,10 @@ document.body.appendChild(container);
 const divs = document.querySelectorAll('.grid');
 divs.forEach(div => {
     div.addEventListener('mouseover', ()=>{
-        const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-        const r = randomBetween(0, 255);
-        const g = randomBetween(0, 255);
-        const b = randomBetween(0, 255);
+        const randomNum = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+        const r = randomNum(0, 255);
+        const g = randomNum(0, 255);
+        const b = randomNum(0, 255);
         const rgb = `rgb(${r},${g},${b})`; 
         div.style.backgroundColor = rgb;
     })
