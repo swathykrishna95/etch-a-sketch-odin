a08ebdc51b31530ed61172fc358dce7c68a9dc5e
Swathy Krishna <138104838+swathykrishna95@users.noreply.github.com>
2023-07-20 14:45:18 +0530

    Add a button to change the grid number

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ let gridNumber = 16;
 const container = document.createElement('div');
 container.setAttribute('style', 'height:100%;display:flex;flex-direction:column');
 
+document.body.style.overflow='hidden';
 
-for(let i=0;i<gridNumber;i++){
+let newGrid = document.createElement('button');
+newGrid.style.textAlign = 'center';
+newGrid.setAttribute('style','width:80px;height:50px');
+newGrid.innerText = "New grid number";
+container.appendChild(newGrid);
+
+
+let createGrid = (gridNumber) =>{
+    for(let i=0;i<gridNumber;i++){
     const row = document.createElement('div');
     row.setAttribute('style', 'display:flex;flex:1');
     for(let j=0;j<gridNumber;j++){
         const div = document.createElement('div');
-        //div.style.flex = '1';
-        //div.style.border = '5px red';
-        //div.style.padding = '5px';
         div.setAttribute('style','flex:1;padding:5px');
         div.classList.add('grid');
         row.appendChild(div);
@@ -27,3 +33,7 @@ divs.forEach(div => {
         div.style.backgroundColor = 'red';
     })
 })
+}
+
+createGrid(gridNumber);
+
