8b1055b25f1bde0a2205aba693ff66d8051f04b5
Swathy Krishna <138104838+swathykrishna95@users.noreply.github.com>
2023-07-20 10:59:19 +0530

    Add a 16 by 16 grid of divs using flexbox

---
+++ script.js
@@ -0,0 +1,29 @@
+let gridNumber = 16;
+const container = document.createElement('div');
+container.setAttribute('style', 'height:100%;display:flex;flex-direction:column;padding:5px auto');
+
+
+
+for(let i=0;i<gridNumber;i++){
+    //const div = document.createElement('div');
+    //div.style.border = '5px red';
+    //div.style.padding = '5px';
+    //row.appendChild(div);
+    const row = document.createElement('div');
+    row.setAttribute('style', 'display:flex;flex:1;padding:0 auto');
+    for(let j=0;j<gridNumber;j++){
+        const div = document.createElement('div');
+        div.style.flex = '1';
+        div.style.border = '5px red';
+        div.style.padding = '5px';
+        
+        row.appendChild(div);
+    }
+    container.appendChild(row);
+    //console.log('row done');
+}
+
+
+
+document.body.appendChild(container);
+
